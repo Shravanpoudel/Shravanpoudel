ebe9e2f03572a25f2734a846f6a8d45d188e7d74
Shravanpoudel <118120128+Shravanpoudel@users.noreply.github.com>
2023-08-27 21:55:48 +0530

    Update Text to speach.py

--- Text to speach.py
+++ Text to speach.py
@@ -1,10 +1,8 @@
-
+#First install pyttsx from pip install 
 import pyttsx3
-
 text =  pyttsx3.init()
 while True:
  t = input("what you want to convert to speech:\n")
-
  text.say(t)
  text.runAndWait()
  if t=="Q":
